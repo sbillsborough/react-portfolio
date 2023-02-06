c2d87d1eaa214aa1b0865c5d679cc78655443628
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-02-06 10:03:54 +0000

    added App function and imported react-router-dom

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,21 @@
+// Imports components
+import { Routes, Route } from "react-router-dom";
+
+// Base App function to bring in the components and routes
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
