0b3313b7f5f34d2630372cfda37474131d277872
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-02-06 10:13:54 +0000

    added resources folder and Home.js file with page content

---
+++ react-portfolio/src/pages/Home.js
@@ -0,0 +1,16 @@
+// import resources
+import headshot from "../resources/img/headshot.jpg";
+
+// Home function with about me section
+
+function Home() {
+  return (
+    <div>
+      <img src={headshot} alt="A picture of Scott Billsborough" />
+      <h1>Web Developer</h1>
+      <p>Hi, I'm Scott and I am a Web Developer. Welcome to my portfolio.</p>
+    </div>
+  );
+}
+
+export default Home;
