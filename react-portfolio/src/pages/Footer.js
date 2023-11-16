1d71625d003c4e6597f8db163f96e437d754a012
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-11-16 18:07:41 +0000

    restructured the footer and added the navlinks

--- react-portfolio/src/pages/Footer.js
+++ react-portfolio/src/pages/Footer.js
@@ -1,8 +1,39 @@
+import { NavLink } from "react-router-dom";
+
 function Footer() {
   return (
-    <div className="footer fixed-bottom">
-      <span>&copy; 2023 Scott Billsborough</span>
-    </div>
+    <footer>
+      <div className="background">
+        <svg></svg>
+      </div>
+      <section>
+        <ul className="socials">
+          <li>
+            <a class="fa-brands fa-linkedin"></a>
+            <a class="fa-brands fa-github"></a>
+            <a class="fa-regular fa-envelope"></a>
+          </li>
+        </ul>
+        <ul className="links">
+          <li>
+            <a href="">
+              <NavLink to="/">Home</NavLink>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <NavLink to="/projects">Projects</NavLink>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <NavLink to="/contact">Contact</NavLink>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <p class="legal">&copy; 2023 Scott Billsborough</p>
+    </footer>
   );
 }
 export default Footer;
