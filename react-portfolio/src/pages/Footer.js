18fea153a713a3a35fdd28c57efafa57507c4227
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-11-16 18:10:03 +0000

    removed the ul li and a as they're not needed

--- react-portfolio/src/pages/Footer.js
+++ react-portfolio/src/pages/Footer.js
@@ -14,23 +14,11 @@ function Footer() {
             <a class="fa-regular fa-envelope"></a>
           </li>
         </ul>
-        <ul className="links">
-          <li>
-            <a href="">
-              <NavLink to="/">Home</NavLink>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <NavLink to="/projects">Projects</NavLink>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <NavLink to="/contact">Contact</NavLink>
-            </a>
-          </li>
-        </ul>
+        <nav className="links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
       </section>
       <p class="legal">&copy; 2023 Scott Billsborough</p>
     </footer>
