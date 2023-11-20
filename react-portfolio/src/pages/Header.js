3d446ea0c043d52288a9e653574898e9493c82e1
AkshatSC <akshatsinghchouhan16@gmail.com>
2023-11-20 20:28:25 +0530

    added animations to header and home component

--- react-portfolio/src/pages/Header.js
+++ react-portfolio/src/pages/Header.js
@@ -1,16 +1,26 @@
 // imports NavLink from React Router
 import { NavLink } from "react-router-dom";
+import React,{useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Header() {
+
+  useEffect(()=>{
+    AOS.init({
+      easing: 'ease',
+  });
+  },[])
+
   return (
     <header>
-      <h3>Scott Billsborough</h3>
+      <h3 data-aos="fade-down" data-aos-delay="600" data-aos-duration="600">Scott Billsborough</h3>
 
       <div className="navbar">
         <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/projects">Projects</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink data-aos="fade-down" data-aos-delay="800" data-aos-duration="600" to="/">Home</NavLink>
+          <NavLink data-aos="fade-down" data-aos-delay="1000" data-aos-duration="600" to="/projects">Projects</NavLink>
+          <NavLink data-aos="fade-down" data-aos-delay="1200" data-aos-duration="600" to="/contact">Contact</NavLink>
         </nav>
       </div>
     </header>
--- react-portfolio/src/pages/Home.js
+++ react-portfolio/src/pages/Home.js
@@ -1,26 +1,36 @@
 // import resources
 import headshot from "../resources/img/headshot.jpg";
+import React,{useEffect} from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // Home function with about me section
 
 function Home() {
+
+  useEffect(()=>{
+    AOS.init({
+      easing: 'ease',
+  });
+  },[])
+
   return (
     <>
       <div className="home-wrapper">
-        <img src={headshot} className="headshot" alt="Scott Billsborough" />
-        <h1>Web Developer</h1>
-        <p>Hi, I'm Scott and I am a Web Developer. Welcome to my portfolio.</p>
+        <img data-aos="flip-left" data-aos-delay="800" data-aos-duration="800" src={headshot} className="headshot" alt="Scott Billsborough" />
+        <h1 data-aos="fade-left" data-aos-delay="1100" data-aos-duration="600">Web Developer</h1>
+        <p data-aos="fade-right" data-aos-delay="1300" data-aos-duration="600">Hi, I'm Scott and I am a Web Developer. Welcome to my portfolio.</p>
       </div>
       <div className="icons-wrapper">
         <div className="icons">
-          <i className="fa-brands fa-html5"></i>
-          <i className="fa-brands fa-css3-alt"></i>
-          <i className="fa-brands fa-js"></i>
-          <i className="fa-brands fa-react"></i>
-          <i className="fa-brands fa-bootstrap"></i>
-          <i className="fa-brands fa-node"></i>
-          <i className="fa-brands fa-npm"></i>
-          <i className="fa-brands fa-github"></i>
+          <i data-aos="flip-down" data-aos-delay="1500" data-aos-duration="600" className="fa-brands fa-html5"></i>
+          <i data-aos="flip-down" data-aos-delay="1700" data-aos-duration="600" className="fa-brands fa-css3-alt"></i>
+          <i data-aos="flip-down" data-aos-delay="1900" data-aos-duration="600" className="fa-brands fa-js"></i>
+          <i data-aos="flip-down" data-aos-delay="2100" data-aos-duration="600" className="fa-brands fa-react"></i>
+          <i data-aos="flip-down" data-aos-delay="2300" data-aos-duration="600" className="fa-brands fa-bootstrap"></i>
+          <i data-aos="flip-down" data-aos-delay="2500" data-aos-duration="600" className="fa-brands fa-node"></i>
+          <i data-aos="flip-down" data-aos-delay="2700" data-aos-duration="600" className="fa-brands fa-npm"></i>
+          <i data-aos="flip-down" data-aos-delay="2900" data-aos-duration="600" className="fa-brands fa-github"></i>
         </div>
       </div>
     </>
