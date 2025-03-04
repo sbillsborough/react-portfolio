a1bf3b52e7beb1b06a4ed5080b8db0a3ed01903d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-03-04 17:15:27 +0000

    BrowserRouter and StrictMode update
    Wrapped the entire App component inside of index.js in BrowserRouter as it's more up-to-date
    Removed the StrictMode to prevent the double-call of components

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,21 @@
 // Imports components
 import { Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
-import Footer from "./pages/Footer";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
 import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
 
 // Base App function to bring in the components and routes
 function App() {
   return (
     <>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-
       <Footer />
     </>
   );
