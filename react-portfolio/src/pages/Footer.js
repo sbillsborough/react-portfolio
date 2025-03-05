ff0f77d1b0071b00f075b864554a9a56de37baea
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-03-05 16:01:57 +0000

    svg is now responsive
    Added preserveAspectRatio='none' which forces the SVG to stretch to 100% width + height
    Ignores the viewBox aspect ratio
    Always covers the full screen width no matter the screen size
    
    object-fit: cover to css to ensure theire's no weird stretching

--- react-portfolio/src/pages/Footer.js
+++ react-portfolio/src/pages/Footer.js
@@ -13,6 +13,7 @@ function Footer() {
           width="100%"
           height="100%"
           viewBox="0 0 1600 900"
+          preserveAspectRatio="none"
         >
           <defs>
             <linearGradient id="bg" x2="0%" y2="100%">
